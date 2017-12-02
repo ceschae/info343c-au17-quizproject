b72ce210246081c3def647b7d66a62b8f0027f11
Claire Kim <hjkim97@outlook.com>
2017-12-01 20:10:00 -0800

    Redirecting between pages all functioning

--- quiz-project/src/App.js
+++ quiz-project/src/App.js
@@ -4,6 +4,8 @@ import logo from './logo.svg';
 import SignInView from "./components/SignIn";
 import SignUpView from "./components/SignUp";
 import SignOutView from "./components/SignOut";
+import Homepage from "./components/Homepage";
+import ProfileView from './components/Profile';
 
 import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
@@ -16,12 +18,14 @@ class App extends Component {
         <h1 className="text-primary display-3">Final Project</h1>
       </header>
       <div className="container">
-        <SignOutView />
         <div className="row">
           <Router>
             <Switch>
               <Route exact path="/" component={SignInView} />
               <Route path="/signup" component={SignUpView} />
+              <Route path="/quiz" component={Homepage} />
+              <Route path="/quiz/:chanName" component={Homepage} />
+              <Route path="/profile" component={Homepage} />
               <Route component={SignInView} />
             </Switch>
           </Router>
