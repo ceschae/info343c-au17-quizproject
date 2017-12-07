5d72b731ecd8beafd5ed373b39a5c9303731d562
Caitlin Schaefer <ceschae@cs.washington.edu>
2017-12-07 00:39:58 -0800

    small features added, including buttons, profile info, etc

---
+++ quiz-project/src/components/AuthoredCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { HashRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+
+import firebase from 'firebase/app';
+
+export default class AuthoredCard extends React.Component {
+    render () {
+        let quiz = this.props.quizSnapshot.val();
+        console.log(this.props.key);
+        console.log(quiz);
+        return (
+            <div className="container pb-3">
+            <div className="card">
+                <div className="card-body">
+                    <h4 className="card-title">{quiz.quizDetails.title}</h4>
+                    <p>{quiz.quizDetails.description}</p>
+                        <button className="btn btn-outline-primary">
+                        <Link to= {{
+                            pathname:'/quiz/' + this.props.key,
+                            state: {quizRef: quiz}}}> 
+                            Click here to take your quiz!</Link></button>
+                </div>
+            </div>
+            </div>
+        );
+    }
+}
